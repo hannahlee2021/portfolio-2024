bbd8edad36c5bfde4f07795e4399c30e8eef4e61
Hannah Lee <106423868+hannahlee2021@users.noreply.github.com>
2024-12-30 17:34:22 -0500

    initial commit

---
+++ titles.js
@@ -0,0 +1,28 @@
+const title = document.querySelector("h1")
+const links = document.querySelectorAll("section a")
+
+const body = document.querySelector("body")
+
+links.forEach((link) => {
+    link.addEventListener("mouseenter", () => {
+        title.innerText = link.getAttribute("data-title")
+
+        body.classList.add("hovered")
+        link.classList.add("hovered")
+
+        body.classList.add(link.getAttribute("data-color"))
+    })
+
+    link.addEventListener("mouseleave", () => {
+        
+        title.innerText = "WONT YOU STAY AWHILE?"
+
+        
+        
+
+        body.classList.remove("hovered")
+        link.classList.remove("hovered")
+
+        body.classList.remove(link.getAttribute("data-color"))
+    })
+})
