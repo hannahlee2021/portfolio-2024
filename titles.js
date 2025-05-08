1babcfda075398a697327187bfa0df16a26e991c
Hannah Lee <106423868+hannahlee2021@users.noreply.github.com>
2025-05-07 23:50:22 -0400

    added scent letter thesis entry, added featured page, added subtitles for featured page

--- titles.js
+++ titles.js
@@ -1,11 +1,14 @@
 const title = document.querySelector("h1")
 const links = document.querySelectorAll("section a")
+const subtitle = document.querySelector("p")
 
 const body = document.querySelector("body")
 
 links.forEach((link) => {
     link.addEventListener("mouseenter", () => {
         title.innerText = link.getAttribute("data-title")
+
+        subtitle.innerText = link.getAttribute("data-subtitle")
 
         body.classList.add("hovered")
         link.classList.add("hovered")
@@ -16,9 +19,7 @@ links.forEach((link) => {
     link.addEventListener("mouseleave", () => {
         
         title.innerText = "WONT YOU STAY AWHILE?"
-
-        
-        
+        subtitle.innerText = ""
 
         body.classList.remove("hovered")
         link.classList.remove("hovered")
