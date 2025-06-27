9c6d39c8aecfcf5290833a2a47b3474f9c59fc4b
Hannah Lee <106423868+hannahlee2021@users.noreply.github.com>
2025-06-27 18:59:00 -0400

    made mobile ver nav bar.... probs needs some tweaks but we do later

--- sideNavClick.js
+++ sideNavClick.js
@@ -1,5 +1,7 @@
 const sidebar = document.querySelector('.bottom-sidebar');
 const content = document.getElementById('sidebar-nav');
+const mobileNav = document.querySelector('.mobile-nav-cont');
+const mobileContent = document.getElementById('mobile-nav');
 
 sidebar.addEventListener('click', function() {
     console.log('clicked');
@@ -7,9 +9,26 @@ sidebar.addEventListener('click', function() {
     if (content.classList.contains('hidden')) {
         content.style.opacity = '1';
         content.classList.remove('hidden');
+        console.log('side opacity 1')
         
     } else {
         content.style.opacity = '0';
         content.classList.add('hidden');
+        console.log('side opacity 0')
+    }
+})
+
+mobileNav.addEventListener('click', function() {
+    console.log('mobile clicked');
+
+    if (mobileContent.classList.contains('hidden')) {
+        mobileContent.style.opacity = '1';
+        mobileContent.classList.remove('hidden');
+
+        console.log('opacity 1')
+    } else {
+        mobileContent.style.opacity = '0';
+        mobileContent.classList.add('hidden');
+        console.log('opacity 0')
     }
 })
