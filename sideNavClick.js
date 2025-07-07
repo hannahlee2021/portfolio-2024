dcae2065a7ebefe0e9d475502f863787852556db
Hannah Lee <106423868+hannahlee2021@users.noreply.github.com>
2025-07-07 16:23:49 -0400

    mobile layout entered content for featured pgand some troubleshooting

--- sideNavClick.js
+++ sideNavClick.js
@@ -24,11 +24,14 @@ mobileNav.addEventListener('click', function() {
     if (mobileContent.classList.contains('hidden')) {
         mobileContent.style.opacity = '1';
         mobileContent.classList.remove('hidden');
-
+        mobileContent.classList.remove('disabled');
         console.log('opacity 1')
+        console.log(mobileContent.classList.value);
     } else {
+        mobileContent.classList.add('disabled');
         mobileContent.style.opacity = '0';
         mobileContent.classList.add('hidden');
         console.log('opacity 0')
+        console.log(mobileContent.classList.value);
     }
 })
